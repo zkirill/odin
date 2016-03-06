cafe70b15b434500821162b596a665688378dc77
Kirill Zdornyy <kirill@dineserve.com>
2016-03-06 13:40:39 -0800

    First code.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@ use std::net::SocketAddr;
 use mio_websocket::interface::*;
 
 fn main() {
-    let mut ws = WebSocket::new("127.0.0.1:8080".parse::<SocketAddr>().unwrap());
+    let mut ws = WebSocket::new("127.0.0.1:2794".parse::<SocketAddr>().unwrap());
+
+    println!("Listening!");
 
     loop {
         match ws.next() {
-            event @ (_, WebSocketEvent::TextMessage(_)) |
-            event @ (_, WebSocketEvent::BinaryMessage(_)) => {
-                ws.send(event);
+            (tok, WebSocketEvent::TextMessage(msg)) => {
+                match ws.get_connected() {
+                    Ok(val) => {
+                        for token in val {
+                            let response = WebSocketEvent::TextMessage(msg.clone());
+                            ws.send((token, response));
+                        }
+                    }
+                    Err(e) => {
+                        panic!(e);
+                    }
+                }
             }
             _ => {}
         }
